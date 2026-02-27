dfa423f2cf2c9b10658f55da50f5960d011b58c3
Naman Sharma <naman123454321@gmail.com>
2026-02-27 06:58:45 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (15.76%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.swift
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.swift
@@ -1,13 +1,19 @@
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
-        var minPrice = prices[0]
         var maxProfit = 0
-        
-        for price in prices {
-            minPrice = min(minPrice, price)
-            maxProfit = max(maxProfit, price - minPrice)
+        var i = 0 // buy 
+        var j = 1 // sell
+
+        while j < prices.count {
+            if prices[i] < prices[j] {
+                let profit = prices[j] - prices[i]
+                maxProfit = max(profit, maxProfit)
+            } else {
+                i = j
+            }
+            j+=1
         }
-        
+
         return maxProfit
     }
 }
